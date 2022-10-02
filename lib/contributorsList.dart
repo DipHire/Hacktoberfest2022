d8fb53602cac7ecf652dca25602b4da6458812df
Jayne Elliott <musicqueen15@yahoo.com>
2022-10-02 17:04:49 +0100

    Added Jayne

--- lib/contributorsList.dart
+++ lib/contributorsList.dart
@@ -14,6 +14,7 @@ List<User> getUsers() {
     {"name": "Aashvi Kothari", "username":"AashviKothari"}, 
     {"name": "Rahul Patnaik", "username": "clichepasta"},
     {"name": "Jay", "username": "jjgajjar04069999"},
+    {"name": "Jayne", "username": "JayneElliott404"},
   ];
 
   return data.map<User>(User.fromJson).toList();
---
+++ participant/JayneElliott404.md
@@ -0,0 +1,5 @@
+Name: Jayne
+
+GitHub: JayneElliott404
+
+Quote: If you want it, work for it
