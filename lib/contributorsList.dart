27b29ac59bde137039ee83f1b631dcc572d5e20a
Sudipta chowdhury <anikc795@gmail.com>
2022-10-07 21:16:19 +0600

    added myself(SudiptaAChy)

--- lib/contributorsList.dart
+++ lib/contributorsList.dart
@@ -50,7 +50,7 @@ List<User> getUsers() {
     {"name": "Saransh Shankar", "username": "Wise-Wizard"},
     {"name": "Joel Sathiyendra Thiyaheswaran", "username": "joelsathi"},
     {"name": "Samarth kumar", "username": "samarthku"},
-    
+    {"name": "Sudipta Chowdhury", "username": "SudiptaAChy"},
   ];
 
   return data.map<User>(User.fromJson).toList();
